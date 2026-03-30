89d41763255a3835cdc6f9f5dc9c6b8701c5593c
ihriyad <ihriyad.dev@gmail.com>
2026-03-31 00:02:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="text-red-600">Hello this is Digitools Platform</h3>
+      <button className="btn btn-outline">Click Me</button>
+    </>
+  );
+}
+
+export default App;
